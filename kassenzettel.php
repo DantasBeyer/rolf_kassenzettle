b9910e7b9335e7bff79b7505c750111499b3f812
DantasBeyer <dantasbeyer@hotmail.com>
2024-04-19 08:22:45 +0200

    first update

--- kassenzettel.php
+++ kassenzettel.php
@@ -24,7 +24,7 @@ if (!isset($vonBerechnung)){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>kassenzettel</title>
+    <title>kassenzettel - Rolf</title>
     <style>
 
     </style>
